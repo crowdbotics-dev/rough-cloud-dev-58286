d2b4698f2b522250fb3cf49d18c5e886325ca1e1
Crowdbotics <engineering@crowdbotics.com>
2023-01-20 03:09:57 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/rough_cloud_dev_58286/MainActivity.java
@@ -0,0 +1,15 @@
+package com.rough_cloud_dev_58286;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "rough_cloud_dev_58286";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "rough-cloud-dev-58286",
+  url: "https://rough-cloud-dev-58286.botics.co",
+  api: "https://rough-cloud-dev-58286.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
